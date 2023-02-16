0fcf67582e3054b02d20f1039d1ba1b33c9042a5
Harsh Pandey <harshsatishpandey@gmail.com>
2023-02-16 22:38:14 +0530

    fix: remappings and default tests

--- tests/Ghost.t.sol
+++ tests/Ghost.t.sol
@@ -3,16 +3,13 @@ pragma solidity ^0.8.0;
 
 import {Test} from 'forge-std/Test.sol';
 
-import {Ghost} from '../src/contracts/Ghost.sol';
-
 contract GhostTest is Test {
-  Ghost public ghost;
 
   function setUp() public {
-    ghost = new Ghost();
+    
   }
 
   function testBoo() public {
-    assertEq(ghost.boo(), 'Boo!');
+    
   }
 }
