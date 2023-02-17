49050543b4400ebfffad5002bb2063bfc0d29d6d
Harsh Pandey <harshsatishpandey@gmail.com>
2023-02-17 19:59:50 +0530

    add: initial tests for arbitrum L2RobotKeeper

--- tests/L2RobotKeeper.t.sol
+++ tests/L2RobotKeeper.t.sol
@@ -29,4 +29,26 @@ contract L2RobotKeeperTest is Test {
       console.log('Final State of ActionsSet 13 after automation', uint256(finalActionsSetState));
     }
   }
+
+  function testSimpleExecuteArbitrum() public {
+    vm.createSelectFork(
+      'https://arb-mainnet.g.alchemy.com/v2/4b5zYcMERmrGAlfneBy2n21k49HK1a-O',
+      49297907 // Dec-28-2022
+    );
+    L2RobotKeeper l2RobotKeeper = new L2RobotKeeper();
+
+    IExecutorBase bridgeExecutor = IExecutorBase(0x7d9103572bE58FfE99dc390E8246f02dcAe6f611);
+    IExecutorBase.ActionsSetState initialActionsSetState = bridgeExecutor.getCurrentState(0);
+    assertEq(uint256(initialActionsSetState), 0);
+    console.log('Initial State of ActionsSet 0', uint256(initialActionsSetState));
+
+    (bool shouldRunKeeper, bytes memory performData) = l2RobotKeeper.checkUpkeep(abi.encode(address(bridgeExecutor)));
+
+    if (shouldRunKeeper) {
+      l2RobotKeeper.performUpkeep(performData);
+      IExecutorBase.ActionsSetState finalActionsSetState = bridgeExecutor.getCurrentState(0);
+      assertEq(uint256(finalActionsSetState), 1);
+      console.log('Final State of ActionsSet 0 after automation', uint256(finalActionsSetState));
+    }
+  }
 }
