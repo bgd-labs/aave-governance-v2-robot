798d925232606fc22977dd62190bace5cebaf29f
Harsh Pandey <harshsatishpandey@gmail.com>
2023-07-13 12:46:54 +0530

    test: l2 keeper refactor and testMultiple

--- tests/L2RobotKeeper.t.sol
+++ tests/L2RobotKeeper.t.sol
@@ -5,10 +5,9 @@ import {Test} from 'forge-std/Test.sol';
 import {L2RobotKeeper} from '../src/contracts/L2RobotKeeper.sol';
 import {IExecutorBase} from 'governance-crosschain-bridges/contracts/interfaces/IExecutorBase.sol';
 import {AaveGovernanceV2} from 'aave-address-book/AaveGovernanceV2.sol';
-import 'forge-std/console.sol';
 
 contract L2RobotKeeperTest is Test {
-  function testSimpleExecutePolygon() public {
+  function testExecutePolygon() public {
     vm.createSelectFork(
       'polygon',
       39099833 // Feb-09-2023
@@ -17,20 +16,33 @@ contract L2RobotKeeperTest is Test {
     L2RobotKeeper l2RobotKeeper = new L2RobotKeeper(address(bridgeExecutor));
 
     IExecutorBase.ActionsSetState actionsSetState = bridgeExecutor.getCurrentState(13);
-    assertEq(uint256(actionsSetState), 0);
-    console.log('Initial State of ActionsSet 13: Queued', uint256(actionsSetState));
+    assertEq(uint256(actionsSetState), uint256(IExecutorBase.ActionsSetState.Queued));
 
     checkAndPerformUpKeep(l2RobotKeeper);
 
     actionsSetState = bridgeExecutor.getCurrentState(13);
-    assertEq(uint256(actionsSetState), 1);
-    console.log(
-      'Final State of ActionsSet 13 after automation: Executed',
-      uint256(actionsSetState)
-    );
+    assertEq(uint256(actionsSetState), uint256(IExecutorBase.ActionsSetState.Executed));
   }
 
-  function testSimpleExecuteArbitrum() public {
+  function testOnlyOneExecuteAtATime() public {
+    vm.createSelectFork(
+      'polygon',
+      37359226 // Dec-28-2023
+    );
+    IExecutorBase bridgeExecutor = IExecutorBase(AaveGovernanceV2.POLYGON_BRIDGE_EXECUTOR);
+    L2RobotKeeper l2RobotKeeper = new L2RobotKeeper(address(bridgeExecutor));
+
+    assertEq(uint256(bridgeExecutor.getCurrentState(9)), uint256(IExecutorBase.ActionsSetState.Queued));
+    assertEq(uint256(bridgeExecutor.getCurrentState(10)), uint256(IExecutorBase.ActionsSetState.Queued));
+
+    checkAndPerformUpKeep(l2RobotKeeper);
+
+    // random execution depends on the blocknumber and timestamp so we know before that only actionSetId 9 is getting executed
+    assertEq(uint256(bridgeExecutor.getCurrentState(9)), uint256(IExecutorBase.ActionsSetState.Executed));
+    assertEq(uint256(bridgeExecutor.getCurrentState(10)), uint256(IExecutorBase.ActionsSetState.Queued));
+  }
+
+  function testExecuteArbitrum() public {
     vm.createSelectFork(
       'arbitrum',
       49297907 // Dec-28-2022
@@ -39,17 +51,15 @@ contract L2RobotKeeperTest is Test {
     L2RobotKeeper l2RobotKeeper = new L2RobotKeeper(address(bridgeExecutor));
 
     IExecutorBase.ActionsSetState actionsSetState = bridgeExecutor.getCurrentState(0);
-    assertEq(uint256(actionsSetState), 0);
-    console.log('Initial State of ActionsSet 0: Queued', uint256(actionsSetState));
+    assertEq(uint256(actionsSetState), uint256(IExecutorBase.ActionsSetState.Queued));
 
     checkAndPerformUpKeep(l2RobotKeeper);
 
     actionsSetState = bridgeExecutor.getCurrentState(0);
-    assertEq(uint256(actionsSetState), 1);
-    console.log('Final State of ActionsSet 0 after automation: Executed', uint256(actionsSetState));
+    assertEq(uint256(actionsSetState), uint256(IExecutorBase.ActionsSetState.Executed));
   }
 
-  function testSimpleExecuteOptimism() public {
+  function testExecuteOptimism() public {
     vm.createSelectFork(
       'optimism',
       84422556 // May-10-2023
@@ -58,14 +68,12 @@ contract L2RobotKeeperTest is Test {
     L2RobotKeeper l2RobotKeeper = new L2RobotKeeper(address(bridgeExecutor));
 
     IExecutorBase.ActionsSetState actionsSetState = bridgeExecutor.getCurrentState(9);
-    assertEq(uint256(actionsSetState), 0);
-    console.log('Initial State of ActionsSet 9: Queued', uint256(actionsSetState));
+    assertEq(uint256(actionsSetState), uint256(IExecutorBase.ActionsSetState.Queued));
 
     checkAndPerformUpKeep(l2RobotKeeper);
 
     actionsSetState = bridgeExecutor.getCurrentState(9);
-    assertEq(uint256(actionsSetState), 1);
-    console.log('Final State of ActionsSet 9 after automation: Executed', uint256(actionsSetState));
+    assertEq(uint256(actionsSetState), uint256(IExecutorBase.ActionsSetState.Executed));
   }
 
   function testDisable() public {
@@ -81,14 +89,12 @@ contract L2RobotKeeperTest is Test {
     vm.stopPrank();
 
     IExecutorBase.ActionsSetState actionsSetState = bridgeExecutor.getCurrentState(9);
-    assertEq(uint256(actionsSetState), 0);
-    console.log('Initial State of ActionsSet 9: Queued', uint256(actionsSetState));
+    assertEq(uint256(actionsSetState), uint256(IExecutorBase.ActionsSetState.Queued));
 
     checkAndPerformUpKeep(l2RobotKeeper);
 
     actionsSetState = bridgeExecutor.getCurrentState(9);
-    assertEq(uint256(actionsSetState), 0);
-    console.log('Final State of ActionsSet 9 after automation: Queued', uint256(actionsSetState));
+    assertEq(uint256(actionsSetState), uint256(IExecutorBase.ActionsSetState.Queued));
   }
 
   function checkAndPerformUpKeep(L2RobotKeeper l2RobotKeeper) private {
