a5a6205ec271f123e63618edc9d00ec78b1f6b12
Harsh Pandey <harshsatishpandey@gmail.com>
2023-08-28 16:14:35 +0530

    chore: increase amount to fund the robot with

--- scripts/DeployEthereumPayload.s.sol
+++ scripts/DeployEthereumPayload.s.sol
@@ -10,7 +10,7 @@ contract Deploy is Script {
 
   address public constant ETHEREUM_ROBOT_OPERATOR = 0x020E452b463568f55BAc6Dc5aFC8F0B62Ea5f0f3;
   uint256 public constant KEEPER_ID = 38708010855340815800266444206792387479170521527111639306025178205742164078384;
-  uint256 public constant AMOUNT_TO_FUND = 300 ether;
+  uint256 public constant AMOUNT_TO_FUND = 600 ether;
 
   function run() external {
     vm.startBroadcast();
