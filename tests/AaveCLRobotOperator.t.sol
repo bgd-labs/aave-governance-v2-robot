eed3f6638349780e1b30f2a62b3e938a376e0832
Harsh Pandey <harshsatishpandey@gmail.com>
2023-11-27 09:47:29 +0530

    fix: tests

--- tests/AaveCLRobotOperator.t.sol
+++ tests/AaveCLRobotOperator.t.sol
@@ -3,31 +3,31 @@ pragma solidity ^0.8.0;
 
 import {Test} from 'forge-std/Test.sol';
 import {AaveCLRobotOperator} from '../src/contracts/AaveCLRobotOperator.sol';
-import {AaveGovernanceV2} from 'aave-address-book/AaveGovernanceV2.sol';
+import {GovernanceV3BNB} from 'aave-address-book/GovernanceV3BNB.sol';
 import {LinkTokenInterface as ILink} from 'chainlink-brownie-contracts/interfaces/LinkTokenInterface.sol';
-import {EthRobotKeeper} from '../src/contracts/EthRobotKeeper.sol';
+import {L2RobotKeeper} from '../src/contracts/L2RobotKeeper.sol';
 import {IKeeperRegistry} from '../src/interfaces/IKeeperRegistry.sol';
 import 'forge-std/console.sol';
 
 contract AaveCLRobotOperatorTest is Test {
   AaveCLRobotOperator public aaveCLRobotOperator;
-  address constant LINK_WHALE = 0xF977814e90dA44bFA03b6295A0616a897441aceC;
-  ILink constant LINK_TOKEN = ILink(0x514910771AF9Ca656af840dff83E8264EcF986CA);
+  address constant LINK_WHALE = 0x51268e001Ac2F53d3d22e2EE24E5af94fA2027ab;
+  ILink constant LINK_TOKEN = ILink(0x404460C6A5EdE2D891e8297795264fDe62ADBB75);
   address constant WITHDRAW_ADDRESS = address(2);
   address constant REGISTRY = 0x02777053d6764996e594c3E88AF1D58D5363a2e6;
   address constant REGISTRAR = 0xDb8e8e2ccb5C033938736aa89Fe4fa1eDfD15a1d;
 
   function setUp() public {
     vm.createSelectFork(
-      'mainnet',
-      17273246 // May-16-2023
+      'bnb',
+      33828457 // Nov-26-2023
     );
     aaveCLRobotOperator = new AaveCLRobotOperator(
       address(LINK_TOKEN),
       REGISTRY,
       REGISTRAR,
       WITHDRAW_ADDRESS,
-      AaveGovernanceV2.SHORT_EXECUTOR
+      GovernanceV3BNB.EXECUTOR_LVL_1
     );
   }
 
@@ -133,12 +133,12 @@ contract AaveCLRobotOperatorTest is Test {
 
   function _registerKeeper() internal returns (uint256, address) {
     vm.startPrank(LINK_WHALE);
-    LINK_TOKEN.transfer(AaveGovernanceV2.SHORT_EXECUTOR, 100 ether);
+    LINK_TOKEN.transfer(GovernanceV3BNB.EXECUTOR_LVL_1, 100 ether);
     vm.stopPrank();
 
-    vm.startPrank(AaveGovernanceV2.SHORT_EXECUTOR);
+    vm.startPrank(GovernanceV3BNB.EXECUTOR_LVL_1);
     LINK_TOKEN.approve(address(aaveCLRobotOperator), 100 ether);
-    EthRobotKeeper ethRobotKeeper = new EthRobotKeeper(address(AaveGovernanceV2.GOV));
+    L2RobotKeeper ethRobotKeeper = new L2RobotKeeper(address(GovernanceV3BNB.EXECUTOR_LVL_1));
     uint256 id = aaveCLRobotOperator.register(
       'testName',
       address(ethRobotKeeper),
